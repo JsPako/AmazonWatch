0e0aed03c2606ec19dfa91549e221b0a2ef16f70
JsPako <jacobswiniarski@gmail.com>
2020-05-23 16:31:22 +0100

    Update Commit

---
+++ client.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# @Author: jacob
+# @Date:   2020-05-23 15:11:29
+# @Last Modified by:   jacob
+# @Last Modified time: 2020-05-23 16:30:26
+
+import establishWindow
+
+
+if __name__ == "__main__":
+    establishWindow.start()
