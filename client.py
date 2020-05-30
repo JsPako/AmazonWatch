e801e454f62c4888e6e9abc39e47eb89e11c85a7
JsPako <jacobswiniarski@gmail.com>
2020-05-30 20:18:35 +0100

    Update Commit

--- client.py
+++ client.py
@@ -1,11 +1,10 @@
 # -*- coding: utf-8 -*-
-# @Author: jacob
-# @Date:   2020-05-23 15:11:29
-# @Last Modified by:   jacob
-# @Last Modified time: 2020-05-23 16:30:26
+# @Author: JsPako, ItsRommels
+# @Webpage: https://github.com/JsPako/AmazonWatch
 
-import establishWindow
-
+import setup
+from mainWindow import startUi
 
 if __name__ == "__main__":
-    establishWindow.start()
+    setup.firstTime()
+    startUi()
